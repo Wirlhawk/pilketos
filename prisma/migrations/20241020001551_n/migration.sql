4bf489494522b131d64d0c10f41bb56fb3be205c
alfriezyi123 <143058555+alfriezyi123@users.noreply.github.com>
2024-10-23 11:28:15 +0700

    Add seed

--- prisma/migrations/20241020001551_n/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `aktif` on the `perolehan` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `perolehan` DROP COLUMN `aktif`;
