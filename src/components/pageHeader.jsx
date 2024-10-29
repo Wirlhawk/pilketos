e04b352addaffb8c55e19aafdaa2c39806d9ab21
Wirlhawk <rayhantogarlumbantobing@gmail.com>
2024-10-29 18:19:38 +0700

    Fix foto paslon

--- src/components/pageHeader.jsx
+++ src/components/pageHeader.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
-export default function PageHeader({icon,title}) {
-  return (
-      <h1 className="text-2xl mb-3 font-semibold text-primary flex items-center gap-3">
-          {icon}
-          {title}
-      </h1>
-  );
+export default function PageHeader({ icon, title }) {
+    return (
+        <h1 className="text-2xl mb-3 font-semibold text-primary flex items-center gap-3">
+            {icon}
+            {title}
+        </h1>
+    );
 }
